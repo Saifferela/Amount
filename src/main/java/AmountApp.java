74fb6327cc115dae7a0e43ef7e13456b3d81c63d
Saifferela <Saifferela@gmail.com>
2021-12-09 21:18:02 +0200

    Refactoring.

--- src/main/java/AmountApp.java
+++ src/main/java/AmountApp.java
@@ -7,12 +7,9 @@ public class AmountApp {
         List<String> words = List.of("Java", "JavaScript", "Python", "C", "Ruby", "Java");
         String word = "Java";
 
-        long amount = new DuplicateFinder(words).findAmount(word);
+        DuplicateFinder duplicateFinder = new DuplicateFinder(words);
+
+        long amount = duplicateFinder.findAmount(word);
         System.out.printf("word %s appears %d times in list %s%n", word, amount, words);
-
-
-        String word1 = "Ruby";
-        long amount1 = new DuplicateFinder(words).findAmount(word1);
-        System.out.printf("word %s appears %d times in list %s%n", word1, amount1, words);
     }
 }
